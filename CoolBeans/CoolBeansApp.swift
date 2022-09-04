17907e26021fc464480e9050d1c269e996ddc016
bekturmamytov <bekturmamytov@gmail.com>
2022-09-04 10:57:04 +0900

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Bektur Mamytov on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
